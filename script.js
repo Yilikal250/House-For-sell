a6de3330c2d726d7cb5172e023d46ca390564d81
Yilikal250 <teshomeyilikal250@gmail.com>
2022-04-21 15:58:03 +0300

    new file with pop up

--- script.js
+++ script.js
@@ -69,3 +69,52 @@ const houseProjects = [
   for (let i = 0; i < 6; i++) {
     renderCards(i);
   }
+
+const cards = document.querySelectorAll('.house-cards');
+const morebtn = document.querySelector('.more');
+const lessbtn = document.querySelector('.less');
+let opened = false;
+
+function moreCards() {
+  if (!opened) {
+    for (let i = 2; i < 6; i += 1) {
+      cards[i].style.display = 'flex';
+    }
+    opened = true;
+    morebtn.classList.toggle('hide-card');
+    lessbtn.classList.toggle('hide-card');
+  } else {
+    for (let i = 2; i < 6; i += 1) {
+      cards[i].style.display = 'none';
+    }
+    opened = false;
+    morebtn.classList.toggle('hide-card');
+    lessbtn.classList.toggle('hide-card');
+  }
+}
+
+const menuBtn = document.querySelector('#mobile-humberg');
+const menuBar = document.querySelector('#popup-1');
+
+let menuClicked = false;
+
+menuBtn.addEventListener('click', () => {
+
+  menuBtn.classList.toggle('popup');
+ 
+    if (!menuClicked) {
+      menuBar.style.display = 'block';
+      menuClicked = true;
+    } else {
+      menuBar.style.display = 'none';
+      menuClicked = false;
+    
+  }
+});
+
+const close = document.querySelector('#close-button');
+
+function togglePopup() {
+  menuBar.style.display = 'none';
+}
+
